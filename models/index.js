476b21a616b7f8f30625fd20d8577f9556aeb64a
shiel1dm <shield55@msu.edu>
2021-10-09 19:53:15 -0400

    added sequalize relationships, not sure on my belongsToMany.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  key: 'id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  key: 'id',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag }) //not sure if I need more than this here.
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
